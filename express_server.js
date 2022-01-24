518d430737b301146fb1c98d042c5d983e20a89b
Seamus McGill <seamusmcgill@gmail.com>
2022-01-24 15:45:09 -0500

    Added endpoint to get urs/new and installed and required body-parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { redirect } = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,6 +28,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {

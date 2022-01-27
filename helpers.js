9c31d58bd75e5b45e1edb5118ab9981cc665f163
Seamus McGill <seamusmcgill@gmail.com>
2022-01-27 13:31:44 -0500

    Created helpers file to store helper functions

---
+++ helpers.js
@@ -0,0 +1,11 @@
+// Helper function to find an email in the users object
+const getUserByEmail = (email, database) => {
+  for (const user in database) {
+    if (email === database[user].email) {
+      return user;
+    }
+  }
+  return false;
+};
+
+module.exports = { getUserByEmail };

4a6d9eadaf120e0f5c1adaec1e1d478a8b80a89a
Seamus McGill <seamusmcgill@gmail.com>
2022-01-27 17:09:42 -0500

    Added tests for all the helper functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail, generateRandomString } = require('../helpers.js');
+const { getUserByEmail, generateRandomString, getUserURLs, addNewUser, authenticateUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -15,8 +15,23 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
+const testURLs = {
+  "Fsyqvq" : {
+    longURL: "http://www.theringer.com",
+    userID: "userRandomID",
+  },
+  "0HNTDg" : {
+    longURL: "http://www.facebook.com",
+    userID: "userRandomID",
+  },
+  "2Hn45s" : {
+    longURL: "http://www.twitter.com",
+    userID: "user3RandomID",
+  },
+};
+
+describe("getUserByEmail", () => {
+  it("should return a user with valid email", function() {
     const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     // Write your assert statement here
@@ -39,5 +54,56 @@ describe("generateRandomString", () => {
   it("should return a string of six characters", () => {
     const randomString = generateRandomString();
     assert.equal(randomString.length, 6);
+  });
+});
+
+describe("getUserURLs", () => {
+  it('should return only the URLs that the user owns', () => {
+    const userURLs = getUserURLs("userRandomID", testURLs);
+    const expectedURLs = ["Fsyqvq", "0HNTDg"];
+    assert.deepEqual(Object.keys(userURLs), expectedURLs);
+  });
+
+  it("should return an empty object if the user owns no URLs", () => {
+    const userURLs = getUserURLs("user2RandomID", testURLs);
+    assert.deepEqual(userURLs, {});
+  });
+});
+
+describe("addNewUser", () => {
+  it("should add a user to the database", () => {
+    const newUser = addNewUser("guy@bro.com", "suhdude", testUsers);
+    const newUserFound = testUsers[newUser] ? true : false;
+    assert.equal(newUserFound, true);
+  });
+
+  it("should return a six character ID", () => {
+    const newUser = addNewUser("guy@bro.com", "suhdude", testUsers);
+    const newUserID = testUsers[newUser].id;
+    assert.equal(newUserID.length, 6);
+  });
+
+  it("should not store the user's password as plaintext", () => {
+    const newUser = addNewUser("guy@bro.com", "suhdude", testUsers);
+    const storedPassword = testUsers[newUser].password;
+    assert.notEqual("suhdude", storedPassword);
+  });
+});
+
+describe("authenticateUser", () => {
+  it("should return the correct six character user ID on successful login", () => {
+    const newUser = getUserByEmail("guy@bro.com", testUsers);
+    const userLoginID = authenticateUser("guy@bro.com", "suhdude", testUsers);
+    assert.equal(newUser, userLoginID);
+  });
+
+  it("should return false if an invalid email is entered", () => {
+    const userLoginAttempt = authenticateUser("guy@dude.com", "suhdude", testUsers);
+    assert.equal(userLoginAttempt, false);
+  });
+
+  it("should return false if an invalid password is entered", () => {
+    const userLoginAttempt = authenticateUser("guy@dude.com", "suhman", testUsers);
+    assert.equal(userLoginAttempt, false);
   });
 });
